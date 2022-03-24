5fafcf02f5d1e705a20df0a8f4fb4551ca9d5a79
Kamer <kuromateusz@gmail.com>
2022-03-24 01:38:08 +0100

    Koniec zadania z labow 3

--- lab_2/src/index.ts
+++ lab_2/src/index.ts
@@ -3,144 +3,161 @@ import { Request, Response } from 'express'
 import { Note } from './note'
 import { Tag } from './tags'
 import fs from 'fs'
+import { isJsxOpeningElement } from 'typescript'
 
 const app = express()
 
-const notes: Note[] = []
-const tagsList: Tag[] = []
-const filePath = "data.json"
+//const notes: Note[] = []
+//const tagsList: Tag[] = []
 
-async function readStorage() {
+async function readStorage(filePath: string) {
 	try {
-		return await fs.promises.readFile(filePath, 'utf-8')
-	} 
-	catch (err) {
-		console.log(err)
-	}
-  }
-
-async function updateStorage(dataToSave : Note[]): Promise<void> {
-	try {
-		await fs.promises.writeFile(filePath, JSON.stringify(dataToSave))
+		const data = await fs.promises.readFile(filePath, 'utf-8')
+		return JSON.parse(data)
 	} catch (err) {
 		console.log(err)
 	}
-  }
+}
+
+async function updateStorage(dataToSave: string, filePath: string): Promise<void> {
+	try {
+		await fs.promises.writeFile(filePath, dataToSave)
+	} catch (err) {
+		console.log(err)
+	}
+}
 
 app.use(express.json())
 
 //#region API Notatki
 // Dodanie notatki
-app.post('/note', function (req: Request, res: Response) {
+app.post('/note', async function (req: Request, res: Response) {
 	console.log('Tworzenie notatki..')
 	console.log(req.body)
-	const storage = readStorage()
-	console.log(storage)
-	const title = req.body.title
-	const content = req.body.content
-	const tagsNames = req.body.tags?.split(',')
+	await readStorage('data/notes.json').then(async notesData => {
+		await readStorage('data/tags.json').then(async tagsData => {
+			const notes: Note[] = notesData
+			const tags: Tag[] = tagsData
+			const title = req.body.title
+			const content = req.body.content
+			const tagsNames = req.body.tags?.split(',')
 
-	if (title == null && content == null) {
-		res.status(400).send('Podano niewłaściwą notatke. Proszę uzupełnić tytuł i zawratość.')
-		return
-	}
-
-	const note = new Note(title, content)
-	let tags: Tag[] = []
-	if (tagsNames?.length > 0) {
-		tagsNames.forEach((tagName: string) => {
-			let existingTag = tagsList.find(x => x.name.toLowerCase() == tagName.toLowerCase().trim())
-			if (existingTag == null) {
-				existingTag = new Tag(tagName.trim())
-				tagsList.push(existingTag)
+			if (title == null && content == null) {
+				res.status(400).send('Podano niewłaściwą notatke. Proszę uzupełnić tytuł i zawratość.')
+				return
 			}
-			tags.push(existingTag)
-		})
-		note.tags = tags
-	}
-	notes.push(note)
-	updateStorage(notes)
 
-	const index = notes.findIndex(x => x.id == note.id)
-	res.status(201).send('Utworzono nową notatkę o ID: ' + note.id + ' (' + index + ')')
+			const note = new Note(title, content)
+			let noteTags: Tag[] = []
+			if (tagsNames?.length > 0) {
+				tagsNames.forEach((tagName: string) => {
+					let existingTag = tags.find(x => x.name.toLowerCase() == tagName.toLowerCase().trim())
+					if (existingTag == null) {
+						existingTag = new Tag(tagName.trim())
+						tags.push(existingTag)
+					}
+					noteTags.push(existingTag)
+				})
+				note.tags = noteTags
+			}
+			notes.push(note)
+			await updateStorage(JSON.stringify(notes, null, 2), 'data/notes.json')
+			await updateStorage(JSON.stringify(tags, null, 2), 'data/tags.json')
+
+			const index = notes.findIndex(x => x.id == note.id)
+			res.status(201).send('Utworzono nową notatkę o ID: ' + note.id + ' (' + index + ')')
+		})
+	})
 })
 
 // Odczytanie notatki
-app.get('/note/:id', function (req: Request, res: Response) {
+app.get('/note/:id', async function (req: Request, res: Response) {
 	console.log('Pobranie notatki..')
 	const id = parseInt(req.params.id)
-	const note = notes.find(x => x.id == id)
-	if (note == null) res.status(404).send('Nie odnaleziono notatki z podanym ID.')
-	else res.status(200).send(note)
+	await readStorage('data/notes.json').then(async notesData => {
+		const notes: Note[] = notesData
+		const note = notes.find(x => x.id == id)
+		if (note == null) res.status(404).send('Nie odnaleziono notatki z podanym ID.')
+		else res.status(200).send(note)
+	})
 })
 
 // Edycja notatki
-app.put('/note/:id', function (req: Request, res: Response) {
+app.put('/note/:id', async function (req: Request, res: Response) {
 	console.log('Edycja notatki..')
 	console.log(req.body)
 	const id = parseInt(req.params.id)
-	const index = notes.findIndex(x => x.id == id)
-	const note = notes[index]
+	await readStorage('data/notes.json').then(async notesData => {
+		await readStorage('data/tags.json').then(async tagsData => {
+			const notes: Note[] = notesData
+			const tags: Tag[] = tagsData
+			const index = notes.findIndex(x => x.id == id)
+			const note = notes[index]
 
-	if (note == null) {
-		res.status(404).send('Nie odnaleziono notatki z podanym ID.')
-		return
-	}
-
-	console.log(note.id)
-	console.log(note.title)
-	if (req.body.title != null) {
-		console.log('1')
-		note.title = req.body.title
-	}
-
-	if (req.body.content != null) note.content = req.body.content
-	console.log('2')
-	if (req.body.tagsNames != null) {
-		console.log('3')
-		const tagsNames = req.body.tagsNames.split(',')
-		let tags: Tag[] = []
-		tagsNames.forEach((tagName: string) => {
-			let existingTag = tagsList.find(x => x.name.toLowerCase() == tagName.toLowerCase())
-			if (existingTag == null) {
-				existingTag = new Tag(tagName)
-				tagsList.push(existingTag)
+			if (note == null) {
+				res.status(404).send('Nie odnaleziono notatki z podanym ID.')
+				return
 			}
-			tags.push(existingTag)
-		})
-		note.tags = tags
-	}
 
-	console.log('4')
-	console.log(index)
-	console.log(note.title)
-	notes[index] = note
-	res.status(204).send()
+			if (req.body.title != null) {
+				note.title = req.body.title
+			}
+
+			if (req.body.content != null) note.content = req.body.content
+
+			if (req.body.tagsNames != null) {
+				const tagsNames = req.body.tagsNames.split(',')
+				let noteTags: Tag[] = []
+				tagsNames.forEach((tagName: string) => {
+					let existingTag = tags.find(x => x.name.toLowerCase() == tagName.toLowerCase().trim())
+					if (existingTag == null) {
+						existingTag = new Tag(tagName.trim())
+						tags.push(existingTag)
+					}
+					noteTags.push(existingTag)
+				})
+				note.tags = noteTags
+			}
+			notes.push(note)
+			await updateStorage(JSON.stringify(notes, null, 2), 'data/notes.json')
+			await updateStorage(JSON.stringify(tags, null, 2), 'data/tags.json')
+
+			notes[index] = note
+			res.status(204).send()
+		})
+	})
 })
 
 // Usunięnice notatki
-app.delete('/note/:id', function (req: Request, res: Response) {
+app.delete('/note/:id', async function (req: Request, res: Response) {
 	console.log('Usuwanie notatki..')
 	const id = parseInt(req.params.id)
-	const index = notes.findIndex(x => x.id == id)
-	if (notes[index] != null) {
-		notes.splice(index, 1)
-		res.status(204).send('Notatka usunięta.')
-	} else res.status(400).send('Nie odnaleziono notatki z podanym ID.')
+	await readStorage('data/notes.json').then(async notesData => {
+		const notes: Note[] = notesData
+		const index = notes.findIndex(x => x.id == id)
+		if (notes[index] != null) {
+			notes.splice(index, 1)
+			await updateStorage(JSON.stringify(notes, null, 2), 'data/notes.json')
+			res.status(204).send('Notatka została usunięta.')
+		} else res.status(400).send('Nie odnaleziono notatki z podanym ID.')
+	})
 })
 
 // Pobranie wszystkich notatek
-app.get('/notes', function (req: Request, res: Response) {
+app.get('/notes', async function (req: Request, res: Response) {
 	console.log('Pobieram liste notatek..')
 	console.log(req.body)
-	if (notes.length > 0) res.status(200).send(notes)
-	else res.status(400).send('Nie ma żadnych notatek.')
+	await readStorage('data/notes.json').then(async notesData => {
+		const notes: Note[] = notesData
+		if (notes.length > 0) res.status(200).send(notes)
+		else res.status(404).send('Nie ma żadnych notatek.')
+	})
 })
 //#endregion
 
 //#region API TagÓW
 // Dodanie nowego tagu
-app.post('/tag', function (req: Request, res: Response) {
+app.post('/tag', async function (req: Request, res: Response) {
 	console.log('Tworzenie tagu..')
 	console.log(req.body)
 	const name: string = req.body.name
@@ -149,67 +166,88 @@ app.post('/tag', function (req: Request, res: Response) {
 		res.status(400).send('Podano niewłaściwy tag. Proszę uzupełnić nazwę.')
 		return
 	}
-	const sameTag = tagsList.find(x => x.id == tag.id)
-	if (sameTag?.name.toLocaleLowerCase() == name.toLocaleLowerCase().trim()) {
-		res.status(400).send('Podany tag już istnieje.')
-		return
-	}
 
-	const tag = new Tag(name.trim())
-	tagsList.push(tag)
+	await readStorage('data/tags.json').then(async tagsData => {
+		const tags: Tag[] = tagsData
+		const sameTag = tags.find(x => x.name?.toLocaleLowerCase() == name.toLocaleLowerCase().trim())
+		if (sameTag != null) {
+			res.status(400).send('Podany tag już istnieje.')
+			return
+		}
 
-	const index = tagsList.findIndex(x => x.id == tag.id)
-	res.status(201).send('Utworzono nowy tag o ID: ' + tag.id + ' (' + index + ')')
+		const tag = new Tag(name.trim())
+		tags.push(tag)
+		await updateStorage(JSON.stringify(tags, null, 2), 'data/tags.json')
+
+		const index = tags.findIndex(x => x.id == tag.id)
+		res.status(201).send('Utworzono nowy tag o ID: ' + tag.id + ' (' + index + ')')
+	})
 })
 
 // Odczytanie tagu
-app.get('/tag/:id', function (req: Request, res: Response) {
+app.get('/tag/:id', async function (req: Request, res: Response) {
 	console.log('Pobranie tagu..')
 	const id = parseInt(req.params.id)
-	const tag = tagsList.find(x => x.id == id)
-	if (tag == null) res.status(404).send('Nie odnaleziono tagu z podanym ID.')
-	else res.status(200).send(tag)
+	await readStorage('data/tags.json').then(async tagsData => {
+		const tags: Tag[] = tagsData
+		const tag = tags.find(x => x.id == id)
+		if (tag == null) res.status(404).send('Nie odnaleziono tagu z podanym ID.')
+		else res.status(200).send(tag)
+	})
 })
 
 // Edycja tagu
-app.put('/tag/:id', function (req: Request, res: Response) {
+app.put('/tag/:id', async function (req: Request, res: Response) {
 	console.log('Edycja tagu..')
 	console.log(req.body)
 	const id = parseInt(req.params.id)
-	const tag = tagsList[id]
+	await readStorage('data/tags.json').then(async tagsData => {
+		const tags : Tag[] = tagsData
+		const index = tags.findIndex(x => x.id == id)
+		const tag = tags[index]
 
-	if (tag == null) {
-		res.status(404).send('Nie odnaleziono tagu z podanym ID.')
-		return
-	}
+		if (tag == null) {
+			res.status(404).send('Nie odnaleziono tagu z podanym ID.')
+			return
+		}
 
-	if (req.body.name != null) {
-		tag.name = req.body.name
-	}
+		if (req.body.name != null) {
+			tag.name = req.body.name
+		}
 
-	tagsList[id] = tag
-	res.status(204).send(tag)
+		tags[index] = tag
+		await updateStorage(JSON.stringify(tags, null, 2), 'data/tags.json')
+
+		res.status(204).send(tag)
+	})
 })
 
 // Usunięnice tagu
-app.delete('/tag/:id', function (req: Request, res: Response) {
+app.delete('/tag/:id', async function (req: Request, res: Response) {
 	console.log('Usuwanie tagu..')
 	const id = parseInt(req.params.id)
-	if (tagsList[id] != null) {
-		tagsList.splice(id, 1)
-		res.status(204).send('Tag usunięty.')
-	} else res.status(400).send('Nie odnaleziono tagu z podanym ID.')
+	await readStorage('data/tags.json').then(async tagsData => {
+		const tags: Tag[] = tagsData
+		const index = tags.findIndex(x => x.id == id)
+		if (tags[index] != null) {
+			tags.splice(index, 1)
+			await updateStorage(JSON.stringify(tags, null, 2), 'data/tags.json')
+			res.status(204).send('Tag został usunięty.')
+		} else res.status(400).send('Nie odnaleziono tagu z podanym ID.')
+	})
 })
 
 // Pobranie wszystkich tagów
-app.get('/tags', function (req: Request, res: Response) {
+app.get('/tags', async function (req: Request, res: Response) {
 	console.log('Pobieram liste tagów..')
-	if (tagsList.length > 0) {
-		console.log(req.body)
-		res.status(200).send(tagsList)
-	}
+	await readStorage('data/tags.json').then(async tagsData => {
+		const tags: Tag[] = tagsData
+		if (tags.length > 0) {
+			console.log(req.body)
+			res.status(200).send(tags)
+		} else res.status(404).send('Nie ma żadnych tagów.')
+	})
 })
 //#endregion
 
 app.listen(3000)
-
