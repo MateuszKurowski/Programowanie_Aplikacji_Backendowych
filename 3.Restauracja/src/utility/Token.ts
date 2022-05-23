ea8bfe18a316c84ccbde62ea6488d416f8bfb3f6
Kamer <kuromateusz@gmail.com>
2022-05-23 20:38:32 +0200

    Token

--- 3.Restauracja/src/utility/Token.ts
+++ 3.Restauracja/src/utility/Token.ts
@@ -1,9 +1,12 @@
 import { sign, verify } from 'jsonwebtoken'
 import { Request } from 'express'
 import { secret } from '../../config.json'
-import { CheckDatabaseLocation } from '../interfaces/database'
+import { EmployeeModel } from '../entities/Employee'
+import { ObjectId } from 'mongoose'
+import { PostionModel } from '../entities/Position'
+
 interface JwtPayload {
-	Id: number
+	Id: ObjectId
 	login: string
 	position: number
 }
@@ -18,16 +21,14 @@ export async function CheckToken(req: Request) {
 
 	try {
 		const payload = verify(token, secret) as JwtPayload
-		await CheckDatabaseLocation()
-			.download('Emplyoee')
-			.then((employeeData: any[]) => {
-				const index = employeeData?.findIndex(x => x?.Id == payload.Id)
-				const user = employeeData[index]
+		await EmployeeModel.find().then((employeeData: any[]) => {
+			const index = employeeData?.findIndex(x => x?.Id == payload.Id)
+			const user = employeeData[index]
 
-				if (user)
-					if (user.login == payload.login) return true
-					else return false
-			})
+			if (user)
+				if (user.login == payload.login) return true
+				else return false
+		})
 	} catch (error) {
 		return false
 	}
@@ -36,4 +37,22 @@ export async function CheckToken(req: Request) {
 export function DownloadPaylod(token: string) {
 	const payload = verify(token, secret) as JwtPayload
 	return payload
+}
+
+export async function CheckPermission(req: Request, positions: string[] = []) {
+	if ((await CheckToken(req)) == false) {
+		throw new Error('Autoryzacja nie powiodła się!')
+	}
+
+	const token = req.headers.authorization?.split(' ')[1]
+	const user = DownloadPaylod(token!)
+	if (positions.length == 0) return user
+	const avaiblePositions = await PostionModel.find()
+	for (const positionName of positions) {
+		const positionId = avaiblePositions.find(x => x.Name.toLowerCase().trim() == positionName.toLowerCase().trim())
+		if (user.Id == positionId) {
+			return user
+		}
+	}
+	throw new Error('Brak uprawnień!')
 }
