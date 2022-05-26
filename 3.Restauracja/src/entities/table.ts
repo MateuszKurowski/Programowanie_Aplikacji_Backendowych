eed78aa43811fa4bf7fdfa47ff50216426deefe7
Kamer <kuromateusz@gmail.com>
2022-05-26 02:37:58 +0200

    Stoliki API

--- 3.Restauracja/src/entities/table.ts
+++ 3.Restauracja/src/entities/table.ts
@@ -1,37 +1,89 @@
-import mongoose, { ObjectId } from 'mongoose'
-import { TableStateModel } from './TableState'
+import mongoose, { Mongoose, ObjectId } from 'mongoose'
+import { ReservationModel } from './Reservation'
 
-export const TableModel = mongoose.model(
-	'Table',
-	new mongoose.Schema(
-		{
-			TableNumber: {
-				type: Number,
-				required: true,
-			},
-			SeatsNumber: {
-				type: Number,
-				required: true,
-				validate(value: number) {
-					if (value < 1) throw new Error('Ilość miejsc przy stoliku nie może być mniejsza niż 1!')
-				},
-			},
-			State: {
-				type: mongoose.Schema.Types.ObjectId,
-				ref: 'TableState',
-				default: async function () {
-					return await TableStateModel.findOne({ Name: 'Wolny' })
-				},
+interface ITable {
+	TableNumber: number
+	SeatsNumber: number
+}
+
+const schema = new mongoose.Schema<ITable>(
+	{
+		TableNumber: {
+			type: Number,
+			required: true,
+			unique: true,
+		},
+		SeatsNumber: {
+			type: Number,
+			required: true,
+			validate(value: number) {
+				if (value < 1) throw new Error('Ilość miejsc przy stoliku nie może być mniejsza niż 1!')
 			},
 		},
-		{ timestamps: true }
-	)
+	},
+	{ timestamps: true }
 )
 
+export const TableModel = mongoose.model<ITable>('Table', schema)
+
 export async function GetTables() {
-	return await TableModel.find()
+	return await TableModel.find({}, '_id TableNumber SeatsNumber')
+}
+
+export async function GetTablesNumbers() {
+	return await TableModel.find({}, 'TableNumber').exec()
 }
 
 export async function GetTableById(Id: ObjectId) {
 	return await TableModel.findById(Id)
+}
+
+export async function GetBusyTablesForNow() {
+	const reservations = await ReservationModel.find({
+		StartDate: { $lte: new Date() },
+		EndDate: { $gte: new Date() },
+	})
+		.select('TableId')
+		.populate('TableId', 'TableNumber')
+		.exec()
+	let tableNumbers: number[] = []
+	for (const reservation of reservations) {
+		const table = reservation.TableId as unknown as ITable
+		const tableNumber = table.TableNumber
+		tableNumbers.push(tableNumber)
+	}
+	return tableNumbers
+}
+
+export async function GetBusyTablesForDate(date: Date) {
+	const reservations = await ReservationModel.find({
+		StartDate: { $lte: new Date(date).setHours(23, 59, 59), $gte: new Date(date).setHours(0, 0, 0) },
+	})
+		.select('TableId')
+		.populate('TableId', 'TableNumber')
+		.exec()
+	let tableNumbers: number[] = []
+	for (const reservation of reservations) {
+		const table = reservation.TableId as unknown as ITable
+		const tableNumber = table.TableNumber
+		tableNumbers.push(tableNumber)
+	}
+	return tableNumbers
+}
+
+export async function GetBusyTablesForFullDate(date: Date) {
+	const reservations = await ReservationModel.find({
+		StartDate: { $lte: new Date(date) },
+		EndDate: { $gte: new Date(date) },
+	})
+		.select('TableId')
+		.populate('TableId', 'TableNumber')
+		.exec()
+	let tableNumbers: number[] = []
+	for (const reservation of reservations) {
+		const table = reservation.TableId as unknown as ITable
+		const tableNumber = table.TableNumber
+		tableNumbers.push(tableNumber)
+	}
+	return tableNumbers
 }
