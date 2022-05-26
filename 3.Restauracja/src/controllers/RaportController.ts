b0bac2bac7df2eef55798efeb947816bea10453c
Kamer <kuromateusz@gmail.com>
2022-05-26 23:20:35 +0200

    Raporty API

--- 3.Restauracja/src/controllers/RaportController.ts
+++ 3.Restauracja/src/controllers/RaportController.ts
@@ -1,5 +1,9 @@
 import { Response, Request } from 'express'
+import { GetOrderByDate, GetOrderByEmployeeId, GetOrderByTableId } from '../entities/Order'
+import { GetTableById, GetTableByNumber, ITable } from '../entities/Table'
 import { CheckPermission } from '../utility/Token'
+import { ObjectId } from 'mongoose'
+import { GetEmployeeById } from '../entities/Employee'
 
 exports.Raport_Get_Orders_Per_Table = async function (req: Request, res: Response) {
 	try {
@@ -14,7 +18,23 @@ exports.Raport_Get_Orders_Per_Table = async function (req: Request, res: Respons
 		}
 	}
 
-	// TODO
+	const tableInput = req.query.number
+	const tableNumber = parseInt(tableInput as string)
+	let orders: any[]
+	if (isNaN(tableNumber)) {
+		const table = (await GetTableByNumber(tableNumber)) as unknown as ITable
+		orders = await GetOrderByTableId(table._id)
+	}
+	{
+		const table = await GetTableById(tableInput as unknown as ObjectId)
+		if (!table) {
+			res.status(500).send('Wystąpił nieoczekiwany błąd. Skontaktuj się z administratorem!')
+			return
+		}
+		orders = await GetOrderByTableId(table._id)
+	}
+	if (orders && orders.length > 0) res.status(200).send(orders)
+	else res.status(404).send('Zapytanie nie zwróciło żadnego wyniku.')
 }
 
 exports.Raport_Get_Orders_Per_Employee = async function (req: Request, res: Response) {
@@ -30,7 +50,17 @@ exports.Raport_Get_Orders_Per_Employee = async function (req: Request, res: Resp
 		}
 	}
 
-	// TODO
+	const employeeInput = req.query.number
+
+	const employee = await GetEmployeeById(employeeInput as unknown as ObjectId)
+	if (!employee) {
+		res.status(500).send('Wystąpił nieoczekiwany błąd. Skontaktuj się z administratorem!')
+		return
+	}
+	const orders = await GetOrderByEmployeeId(employee._id)
+
+	if (orders && orders.length > 0) res.status(200).send(orders)
+	else res.status(404).send('Zapytanie nie zwróciło żadnego wyniku.')
 }
 
 exports.Raport_Get_Orders_In_Time = async function (req: Request, res: Response) {
@@ -46,7 +76,24 @@ exports.Raport_Get_Orders_In_Time = async function (req: Request, res: Response)
 		}
 	}
 
-	// TODO
+	const startDateString = req.query.startdate as string
+	const endDateString = req.query.enddate as string
+	let startDate: Date
+	let endDate: Date
+	try {
+		startDate = new Date(startDateString)
+		endDate = new Date(endDateString)
+	} catch (error: any) {
+		res.status(400).send({
+			Message: 'Podane daty są nieprawidłowe!',
+			Error: error.message,
+		})
+		return
+	}
+	const orders = await GetOrderByDate(startDate, endDate)
+
+	if (orders && orders.length > 0) res.status(200).send(orders)
+	else res.status(404).send('Zapytanie nie zwróciło żadnego wyniku.')
 }
 
 exports.Raport_Get_Cash_In_Time = async function (req: Request, res: Response) {
@@ -62,5 +109,34 @@ exports.Raport_Get_Cash_In_Time = async function (req: Request, res: Response) {
 		}
 	}
 
-	// TODO
+	const startDateString = req.query.startdate as string
+	const endDateString = req.query.enddate as string
+	let startDate: Date
+	let endDate: Date
+	try {
+		startDate = new Date(startDateString)
+		endDate = new Date(endDateString)
+	} catch (error: any) {
+		res.status(400).send({
+			Message: 'Podane daty są nieprawidłowe!',
+			Error: error.message,
+		})
+		return
+	}
+	const orders = await GetOrderByDate(startDate, endDate)
+
+	let calculatedPrice: number = 0
+	for (const order of orders)
+	{
+		const price = order.Price
+		if (price) calculatedPrice += price
+	}
+
+	if (orders && orders.length > 0) res.status(200).send(
+		{
+			Message: 'Okresowy przychód wyniósł: ' + calculatedPrice,
+			Orders: orders
+		}
+	)
+	else res.status(404).send('Zapytanie nie zwróciło żadnego wyniku.')
 }
--- 3.Restauracja/src/handlers/RaportHandler.ts
+++ 3.Restauracja/src/handlers/RaportHandler.ts
@@ -1,9 +1,9 @@
-// const routerRaport = require('express').Router()
-// const Raport_Controller = require('../Controllers/RaportController')
+const routerRaport = require('express').Router()
+const Raport_Controller = require('../Controllers/RaportController')
 
-// routerRaport
-// 	.get('/', Raport_Controller.Raport_Get_Orders_Per_Table)
-// 	.get('/', Raport_Controller.Raport_Get_Orders_Per_Employee)
-// 	.get('/', Raport_Controller.Raport_Get_Orders_In_Time)
-// 	.get('/', Raport_Controller.Raport_Get_Cash_In_Time)
-// module.exports = routerRaport
+routerRaport
+	.get('/', Raport_Controller.Raport_Get_Orders_Per_Table)
+	.get('/', Raport_Controller.Raport_Get_Orders_Per_Employee)
+	.get('/', Raport_Controller.Raport_Get_Orders_In_Time)
+	.get('/', Raport_Controller.Raport_Get_Cash_In_Time)
+module.exports = routerRaport
