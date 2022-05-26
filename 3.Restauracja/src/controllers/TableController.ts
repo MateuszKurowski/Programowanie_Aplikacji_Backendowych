86b9927fa51e4c2e272a896ac7691757721b2e24
Kamer <kuromateusz@gmail.com>
2022-05-26 02:34:56 +0200

    Stoliki API

--- 3.Restauracja/src/controllers/TableController.ts
+++ 3.Restauracja/src/controllers/TableController.ts
@@ -1,22 +1,184 @@
 import { Response, Request } from 'express'
 import { CheckPermission } from '../utility/Token'
-import { GetTables, GetTableById, TableModel } from '../entities/Table'
+import {
+	GetTables,
+	GetTableById,
+	TableModel,
+	GetBusyTablesForNow,
+	GetTablesNumbers,
+	GetBusyTablesForDate,
+	GetBusyTablesForFullDate,
+} from '../entities/Table'
 import { ObjectId } from 'mongoose'
 
 exports.Table_Get_All = async function (req: Request, res: Response) {
 	try {
-		CheckPermission(req, [''])
-	} catch (err: any) {
-		if (err._message == 'Autoryzacja nie powiodła się!') {
-			res.status(401).send(err._message)
+		await CheckPermission(req, [''])
+	} catch (error: any) {
+		if (error.message == 'Autoryzacja nie powiodła się!') {
+			res.status(401).send(error.message)
 			return
 		}
-		if (err._message == 'Brak uprawnień!') {
-			res.status(403).send(err._message)
+		if (error.message == 'Brak uprawnień!') {
+			res.status(403).send(error.message)
 		}
 	}
 
-	const tables = await GetTables()
+	let tables: any
+	const sort = (req.query.sort as string) ?? 'null'
+	const sortBy = (req.query.sortby as string) ?? 'null'
+	if (sort) {
+		switch (sort.toLowerCase()) {
+			default:
+			case 'desc':
+				switch (sortBy.toLowerCase()) {
+					case 'seats':
+						tables = (await GetTables()).sort((one, two) => (one.SeatsNumber > two.SeatsNumber ? -1 : 1))
+						break
+					case 'number':
+					default:
+						tables = (await GetTables()).sort((one, two) => (one.TableNumber > two.TableNumber ? -1 : 1))
+						break
+				}
+				break
+			case 'asc':
+				switch (sortBy.toLowerCase()) {
+					case 'seats':
+						tables = (await GetTables()).sort((one, two) => (one.SeatsNumber < two.SeatsNumber ? -1 : 1))
+						break
+					case 'number':
+					default:
+						tables = (await GetTables()).sort((one, two) => (one.TableNumber < two.TableNumber ? -1 : 1))
+						break
+				}
+				break
+		}
+	} else {
+		tables = await GetTables()
+	}
+
+	if (!tables) {
+		res.status(204).send('Tabela jest pusta.')
+	} else {
+		res.status(200).send(tables)
+	}
+}
+
+exports.Table_Get_All_With_Status = async function (req: Request, res: Response) {
+	try {
+		await CheckPermission(req, [''])
+	} catch (error: any) {
+		if (error.message == 'Autoryzacja nie powiodła się!') {
+			res.status(401).send(error.message)
+			return
+		}
+		if (error.message == 'Brak uprawnień!') {
+			res.status(403).send(error.message)
+		}
+	}
+
+	const busyTablesNumbers = await GetBusyTablesForNow()
+	const tablesNumbers = await GetTablesNumbers()
+	const availableTablesNumbers = tablesNumbers
+		.filter(table => busyTablesNumbers.indexOf(table.TableNumber) < 0)
+		.map(table => table.TableNumber)
+	const busyTables = await TableModel.find({
+		TableNumber: { $in: busyTablesNumbers },
+	}).exec()
+	const availableTables = await TableModel.find({
+		TableNumber: { $in: availableTablesNumbers },
+	}).exec()
+	const result = { BusyTables: busyTables, AvailableTables: availableTables }
+
+	if (!result) {
+		res.status(204).send('Tabela jest pusta.')
+	} else {
+		res.status(200).send(result)
+	}
+}
+
+exports.Table_Get_All_Available = async function (req: Request, res: Response) {
+	try {
+		await CheckPermission(req, [''])
+	} catch (error: any) {
+		if (error.message == 'Autoryzacja nie powiodła się!') {
+			res.status(401).send(error.message)
+			return
+		}
+		if (error.message == 'Brak uprawnień!') {
+			res.status(403).send(error.message)
+		}
+	}
+
+	let busyTablesNumbers: any
+	const date = req.query.date
+	if (date) {
+		try {
+			const validDate = new Date(date as string)
+			if (validDate.getHours() == 0) {
+				busyTablesNumbers = await GetBusyTablesForDate(validDate)
+			} else {
+				busyTablesNumbers = await GetBusyTablesForFullDate(validDate)
+			}
+		} catch (error) {
+			res.status(400).send('Podano nieprawidłowy format daty!')
+		}
+	} else {
+		busyTablesNumbers = await GetBusyTablesForNow()
+	}
+
+	const tablesNumbers = await GetTablesNumbers()
+	const availableTables = tablesNumbers
+		.filter(table => busyTablesNumbers.indexOf(table.TableNumber) < 0)
+		.map(table => table.TableNumber)
+
+	const seats = req.query.seats
+	let tables: any
+	if (seats) {
+		tables = await TableModel.find({
+			TableNumber: { $in: availableTables },
+			SeatsNumber: +seats,
+		}).exec()
+	} else {
+		tables = await TableModel.find({
+			TableNumber: { $in: availableTables },
+		}).exec()
+	}
+
+	if (!tables) {
+		res.status(204).send('Tabela jest pusta.')
+	} else {
+		res.status(200).send(tables)
+	}
+}
+
+exports.Table_Get_All_Busy = async function (req: Request, res: Response) {
+	try {
+		await CheckPermission(req, [''])
+	} catch (error: any) {
+		if (error.message == 'Autoryzacja nie powiodła się!') {
+			res.status(401).send(error.message)
+			return
+		}
+		if (error.message == 'Brak uprawnień!') {
+			res.status(403).send(error.message)
+		}
+	}
+
+	const busyTablesNumbers = await GetBusyTablesForNow()
+
+	const seats = req.query.seats
+	let tables: any
+	if (seats) {
+		tables = await TableModel.find({
+			TableNumber: { $in: busyTablesNumbers },
+			SeatsNumber: +seats,
+		}).exec()
+	} else {
+		tables = await TableModel.find({
+			TableNumber: { $in: busyTablesNumbers },
+		}).exec()
+	}
 
 	if (!tables) {
 		res.status(204).send('Tabela jest pusta.')
@@ -27,21 +189,23 @@ exports.Table_Get_All = async function (req: Request, res: Response) {
 
 exports.Table_Post = async function (req: Request, res: Response) {
 	try {
-		CheckPermission(req, [''])
-	} catch (err: any) {
-		if (err._message == 'Autoryzacja nie powiodła się!') {
-			res.status(401).send(err._message)
+		await CheckPermission(req, ['Księgowy, Szef, Zastępca szefa'])
+	} catch (error: any) {
+		if (error.message == 'Autoryzacja nie powiodła się!') {
+			res.status(401).send(error.message)
 			return
 		}
-		if (err._message == 'Brak uprawnień!') {
-			res.status(403).send(err._message)
+		if (error.message == 'Brak uprawnień!') {
+			res.status(403).send(error.message)
 		}
 	}
 
 	const name = req.body.Name
+	const seatsNumber = req.body.SeatsNumber
 	try {
-		const table = await new TableModel({
+		const table = new TableModel({
 			Name: name,
+			SeatsNumber: seatsNumber,
 		})
 		await table.save()
 		res.status(201).send({
@@ -51,21 +215,21 @@ exports.Table_Post = async function (req: Request, res: Response) {
 	} catch (error: any) {
 		res.status(500).send({
 			Message: 'Dodawanie nie powiodło się!',
-			Error: error._message,
+			error: error.message,
 		})
 	}
 }
 
 exports.Table_Get = async function (req: Request, res: Response) {
 	try {
-		CheckPermission(req, [''])
-	} catch (err: any) {
-		if (err._message == 'Autoryzacja nie powiodła się!') {
-			res.status(401).send(err._message)
+		await CheckPermission(req, [''])
+	} catch (error: any) {
+		if (error.message == 'Autoryzacja nie powiodła się!') {
+			res.status(401).send(error.message)
 			return
 		}
-		if (err._message == 'Brak uprawnień!') {
-			res.status(403).send(err._message)
+		if (error.message == 'Brak uprawnień!') {
+			res.status(403).send(error.message)
 		}
 	}
 
@@ -82,14 +246,14 @@ exports.Table_Get = async function (req: Request, res: Response) {
 
 exports.Table_Put = async function (req: Request, res: Response) {
 	try {
-		CheckPermission(req, [''])
-	} catch (err: any) {
-		if (err._message == 'Autoryzacja nie powiodła się!') {
-			res.status(401).send(err._message)
+		await CheckPermission(req, ['Księgowy, Szef, Zastępca szefa'])
+	} catch (error: any) {
+		if (error.message == 'Autoryzacja nie powiodła się!') {
+			res.status(401).send(error.message)
 			return
 		}
-		if (err._message == 'Brak uprawnień!') {
-			res.status(403).send(err._message)
+		if (error.message == 'Brak uprawnień!') {
+			res.status(403).send(error.message)
 		}
 	}
 
@@ -100,32 +264,38 @@ exports.Table_Put = async function (req: Request, res: Response) {
 	if (!table) {
 		res.status(404).send('Nie odnaleziono rekordu z podanym ID.')
 	} else {
-		await TableModel.updateOne(
-			{ _id: id },
-			{
-				$set: {
-					Name: req.body.Name,
-				},
-			}
-		)
-		table.Name = req.body.Name
-		res.status(200).send({
-			Message: 'Operacja powiodła się.',
-			Table: table,
-		})
+		try {
+			await TableModel.updateOne(
+				{ _id: id },
+				{
+					$set: {
+						TableNumber: req.body.TableNumber,
+						SeatsNumber: req.body.SeatsNumber,
+					},
+				}
+			)
+			table!.TableNumber = req.body.TableNumber
+			table!.SeatsNumber = req.body.SeatsNumber
+			res.status(200).send({
+				Message: 'Operacja powiodła się.',
+				Table: table,
+			})
+		} catch (error: any) {
+			res.status(400).send(error.message)
+		}
 	}
 }
 
 exports.Table_Delete = async function (req: Request, res: Response) {
 	try {
-		CheckPermission(req, [''])
-	} catch (err: any) {
-		if (err._message == 'Autoryzacja nie powiodła się!') {
-			res.status(401).send(err._message)
+		await CheckPermission(req, ['Księgowy, Szef, Zastępca szefa'])
+	} catch (error: any) {
+		if (error.message == 'Autoryzacja nie powiodła się!') {
+			res.status(401).send(error.message)
 			return
 		}
-		if (err._message == 'Brak uprawnień!') {
-			res.status(403).send(err._message)
+		if (error.message == 'Brak uprawnień!') {
+			res.status(403).send(error.message)
 		}
 	}
 
@@ -136,7 +306,11 @@ exports.Table_Delete = async function (req: Request, res: Response) {
 	if (!table) {
 		res.status(404).send('Nie odnaleziono rekordu z podanym ID.')
 	} else {
-		await TableModel.deleteOne({ _id: id })
-		res.status(200).send('Rekord został usunięty.')
+		try {
+			await TableModel.deleteOne({ _id: id })
+			res.status(200).send('Rekord został usunięty.')
+		} catch (error: any) {
+			res.status(500).send(error.message)
+		}
 	}
 }
--- 3.Restauracja/src/handlers/TableHandler.ts
+++ 3.Restauracja/src/handlers/TableHandler.ts
@@ -3,6 +3,9 @@ const Table_Controller = require('../Controllers/TableController')
 
 routerTable
 	.get('/list', Table_Controller.Table_Get_All)
+	.get('/list/status', Table_Controller.Table_Get_All_With_Status)
+	.get('/list/available', Table_Controller.Table_Get_All_Available)
+	.get('/list/busy', Table_Controller.Table_Get_All_Busy)
 	.post('/', Table_Controller.Table_Post)
 	.get('/:id', Table_Controller.Table_Get)
 	.put('/:id', Table_Controller.Table_Put)
